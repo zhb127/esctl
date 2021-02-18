dc7fd01ba4df8912f3b07ee98863cf2cb54f80dd
bocai <1224680008@qq.com>
2021-02-18 18:02:54 +0800

    feat: index reindex

---
+++ internal/index/reindex/handler.go
@@ -0,0 +1,63 @@
+package reindex
+
+import (
+	"encoding/json"
+	"esctl/internal/index/app"
+	"esctl/pkg/es"
+	"esctl/pkg/log"
+	"fmt"
+
+	"github.com/spf13/pflag"
+)
+
+type IHandler interface {
+	Handle(flags *HandlerFlags) error
+	ParseCmdFlags(cmdFlags *pflag.FlagSet) (*HandlerFlags, error)
+}
+
+type handler struct {
+	logHelper log.IHelper
+	esHelper  es.IHelper
+}
+
+func NewHandler(a app.IApp) IHandler {
+	return &handler{
+		logHelper: a.LogHelper(),
+		esHelper:  a.ESHelper(),
+	}
+}
+
+type HandlerFlags struct {
+	SrcIndexName  string
+	DestIndexName string
+}
+
+func (h *handler) Handle(flags *HandlerFlags) error {
+	resp, err := h.esHelper.Reindex(flags.SrcIndexName, flags.DestIndexName)
+	if err != nil {
+		return err
+	}
+
+	jsonBytes, _ := json.Marshal(resp)
+	fmt.Println(string(jsonBytes))
+
+	return nil
+}
+
+func (h *handler) ParseCmdFlags(cmdFlags *pflag.FlagSet) (*HandlerFlags, error) {
+	handlerFlags := &HandlerFlags{}
+
+	flagSrcIndexName, err := cmdFlags.GetString("src")
+	if err != nil {
+		return nil, err
+	}
+	handlerFlags.SrcIndexName = flagSrcIndexName
+
+	flagDestIndexName, err := cmdFlags.GetString("dest")
+	if err != nil {
+		return nil, err
+	}
+	handlerFlags.DestIndexName = flagDestIndexName
+
+	return handlerFlags, nil
+}
